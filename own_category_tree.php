546b9bba81f546baa1a463030ff55cfcada2c1f3
dshovchko <d.shovchko@gmail.com>
2016-12-22 20:55:29 +0200

    Update own_category_tree for 4.6.2

--- own_category_tree.php
+++ own_category_tree.php
@@ -1,26 +1,75 @@
 <?php
- 
-$plugin['version'] = '0.0.1';
+
+// This is a PLUGIN TEMPLATE.
+
+// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
+// run this file at the command line to produce a plugin for distribution:
+// $ php abc_myplugin.php > abc_myplugin-0.1.txt
+
+// Plugin name is optional.  If unset, it will be extracted from the current
+// file name. Plugin names should start with a three letter prefix which is
+// unique and reserved for each plugin author ('abc' is just an example).
+// Uncomment and edit this line to override:
+# $plugin['name'] = 'abc_plugin';
+
+// Allow raw HTML help, as opposed to Textile.
+// 0 = Plugin help is in Textile format, no raw HTML allowed (default).
+// 1 = Plugin help is in raw HTML.  Not recommended.
+# $plugin['allow_html_help'] = 0;
+
+$plugin['version'] = '4.6.2.20161213';
 $plugin['author'] = 'Dmitry Shovchko';
 $plugin['author_uri'] = 'http://github.com/dshovchko/own_category_tree';
 $plugin['description'] = 'Creates a hierarchical list of categories';
-$plugin['type'] = '1';
-$plugin['allow_html_help'] = '1';
- 
+
+// Plugin load order:
+// The default value of 5 would fit most plugins, while for instance comment
+// spam evaluators or URL redirectors would probably want to run earlier
+// (1...4) to prepare the environment for everything else that follows.
+// Values 6...9 should be considered for plugins which would work late.
+// This order is user-overrideable.
+# $plugin['order'] = 5;
+
+// Plugin 'type' defines where the plugin is loaded
+// 0 = public       : only on the public side of the website (default)
+// 1 = public+admin : on both the public and non-AJAX admin side
+// 2 = library      : only when include_plugin() or require_plugin() is called
+// 3 = admin        : only on the non-AJAX admin side
+// 4 = admin+ajax   : only on admin side
+// 5 = public+admin+ajax   : on both the public and admin side
+$plugin['type'] = 1;
+
+// Plugin 'flags' signal the presence of optional capabilities to the core plugin loader.
+// Use an appropriately OR-ed combination of these flags.
+// The four high-order bits 0xf000 are available for this plugin's private use.
+if (!defined('PLUGIN_HAS_PREFS')) define('PLUGIN_HAS_PREFS', 0x0001); // This plugin wants to receive "plugin_prefs.{$plugin['name']}" events
+if (!defined('PLUGIN_LIFECYCLE_NOTIFY')) define('PLUGIN_LIFECYCLE_NOTIFY', 0x0002); // This plugin wants to receive "plugin_lifecycle.{$plugin['name']}" events
+
+$plugin['flags'] = '0';
+
+
 if (!defined('txpinterface'))
-	@include_once('zem_tpl.php');
- 
-if(0){
+    @include_once('zem_tpl.php');
+
+if (0) {
 ?>
 # --- BEGIN PLUGIN HELP ---
-
-<!-- *** BEGIN PLUGIN CSS *** -->
-<!-- *** END PLUGIN CSS *** -->
 
 # --- END PLUGIN HELP ---
 <?php
 }
+
 # --- BEGIN PLUGIN CODE ---
+
+### TAG REGISTRATION ###
+########################
+
+Txp::get('\Textpattern\Tag\Registry')
+ ->register('own_category_tree');
+
+
+### PRIMARY TAG FUNCTIONS ###
+#############################
 
 function own_category_tree($atts) {
 	global $pretext;
@@ -161,6 +210,6 @@ function own_category_tree_link($category, $section, $title)
 function own_category_tree_tab($l) {
 	return str_repeat("\t", $l);
 }
-
 # --- END PLUGIN CODE ---
+
 ?>
